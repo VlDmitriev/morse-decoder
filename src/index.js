7d5b3b7a1160ce12e6d4126cf9cb32d96cf7b609
VlDmitriev <dmitriev.vlad98@yandex.ru>
2022-02-22 22:50:06 +0400

    complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let resultString = '';
+    let symbol = '';
+    for (let i = 0; i < expr.length; i += 10){
+        let string = expr.slice(i, i+10);
+        if (string == '**********'){
+            resultString = resultString + ' '
+            continue
+        }
+
+        for (let j = 0; j < 10; j += 2){
+            let str = string.slice(j, j+2);
+            if (str === '10'){
+                symbol = symbol + '.';
+            }else if (str === '11'){
+                symbol = symbol + '-';
+                
+            }
+        }
+        
+        resultString = resultString + MORSE_TABLE[symbol]
+        symbol = ''
+    }
+    return resultString
 }
 
 module.exports = {
